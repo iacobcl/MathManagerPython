2f0e8514654d29d5c1404e4da9241ec1e9266af6
Claudia Iacob <iacob.claudia@gmail.com>
2025-03-28 11:06:05 +0000

    Practical week 9 2024-2025

--- mathmanager.py
+++ mathmanager.py
@@ -8,3 +8,45 @@ class mathmanager:
 
 	def multiply(self, a, b):
 			return a*b
+	
+	def interest(self, deposit, term):
+		if deposit <= 0:
+			return -1
+		if term == 1:
+			return (0.038*deposit)/12
+		elif term == 2:
+				return (0.036*deposit)/12
+		else:
+				return -2
+
+	def tax(self, income):
+		if (income >= 0 and income <= 12570):
+			return 0
+		elif (income > 12570 and income <= 50270):
+			return 20/100*(income-12570)
+		elif (income >50270 and income <= 125140):
+			return 20/100*(50270-12570) + 40/100*(income-50270)
+		elif (income > 125140):
+			return 20/100*(50270-12570) + 40/100*(125140-50270) + 45/100*(income-125140)
+		else:
+			return -1
+		
+
+	def degree(self, fyp, comp1, comp2, opt1, opt2):
+		if (fyp < 0 or comp1 < 0 or comp2 <0 or opt1 < 0 or opt2 < 0):
+			return -1
+		if (fyp > 100 or comp1 > 100 or comp2 > 100 or opt1 > 100 or opt2 > 100):
+			return -1
+		m = min(comp1, comp2, opt1, opt2)
+		avg = (fyp + comp1 + comp2 + opt1 + opt2 - m) / 4
+		if avg >=70:
+			return "first"
+		elif (avg < 70 and avg >= 60):
+			return "2:1"
+		elif (avg < 60 and avg >= 50):
+			return "2:2"
+		elif (avg < 50 and avg >= 40):
+			return "third"
+		else:
+			return "fail"
+			
--- mathmanagertest.py
+++ mathmanagertest.py
@@ -13,3 +13,65 @@ class mathmanagertest(unittest.TestCase):
 	def testMultiply(self):
 		math = mathmanager()
 		self.assertEqual(math.multiply(0, 3), 0)
+
+	def testInterest(self):
+		math = mathmanager()
+		self.assertEqual(round(math.interest(1000, 1)), 3)
+
+	#Interest rate
+	#positive amount, term = 2 
+	def testInterest1(self):
+		math = mathmanager()
+		self.assertEqual(math.interest(1000, 2), 3)
+	
+	#positive amount, term = 3 (invalid) 
+	def testInterest2(self):
+		math = mathmanager()
+		self.assertEqual(math.interest(1000, 3), -2)
+		
+	#negative amount, term = 1	
+	def testInterest3(self):
+		math = mathmanager()
+		self.assertEqual(math.interest(-1000, 1), -1)
+
+	#add: positive amount, term = 1, 3, 0, -1
+	# negative amount, term = 2, -1, 0, 3
+	# zero amount, term = 1, 2, 3, 0, -1	
+
+
+	#Tax calculator
+	#income in first bracket
+	def testTax(self):
+		math = mathmanager()
+		self.assertEqual(math.tax(10000), 0)
+
+	#income in second bracket
+	def testTax1(self):
+		math = mathmanager()
+		self.assertEqual(math.tax(15000), 486)
+
+	#income in third bracket
+	def testTax2(self):
+		math = mathmanager()
+		self.assertEqual(math.tax(52000), 8232)
+	
+	#add income in all brackets, negative income, income equals each threashold
+
+
+	#Degree classification calculator
+	#combination of grades that get a 2:1
+	def testDegree(self):
+		math = mathmanager()
+		self.assertEqual(math.degree(40, 70, 70, 55, 60), "2:1")
+
+	#combination of grades that get a third
+	def testDegree1(self):
+		math = mathmanager()
+		self.assertEqual(math.degree(0, 70, 50, 60, 40), "third")
+
+	#at least one negative grade
+	def testDegree2(self):
+		math = mathmanager()
+		self.assertEqual(math.degree(-1, 40, 40, 40, 40), -1)
+
+	#add at least one grade higher than 100, combination of grades that get a fist, 2:2, fail
